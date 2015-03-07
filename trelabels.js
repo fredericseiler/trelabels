48c9f64c3c0a2a23a158ca8dc620f30581ad8df2
Frederic Seiler <fredericseiler@coodea.com>
2015-03-07 11:34:04 +0100

    Persistent settings

--- trelabels.js
+++ trelabels.js
@@ -1,43 +1,51 @@
-$(function() {
+$(function()
+{
+  var button = '<a href="#" id="trelabels" class="header-btn"><span class="header-btn-icon icon-lg icon-label light"></span></a>';
   $('head').append('<link href="//fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">');
-  
-  var button = '<a href="#" id="trelabels" class="board-header-btn" title="Show label names">'
-              +'<span class="board-header-btn-icon icon-sm icon-label"></span></a>';
-  
-  function rebuild() {
-    if ($('body').hasClass('body-board-view') && ! $('#trelabels').hasClass('board-header-btn'))
-    {
-      $('.board-header-btns.mod-left').append(button);
-      
-      if ($('body').hasClass('trelabels'))
-      {
-        $('#trelabels').addClass('board-header-btn-enabled');
-        $('#trelabels').attr('title', 'Hide label names');
-      }
-    }
+
+  function showLabels()
+  {
+    $('body:not(.trelabels)').addClass('trelabels');
+    $('#trelabels:not(.active)').addClass('active').attr('title', 'Hide label names');
+    if (typeof localStorage !== 'undefined') localStorage.setItem('trelabels', "1");
   }
-  
+
+  function hideLabels()
+  {
+    $('body.trelabels').removeClass('trelabels');
+    $('#trelabels.active').removeClass('active').attr('title', 'Show label names');
+    if (typeof localStorage !== 'undefined') localStorage.setItem('trelabels', "0");
+  }
+
+  function rebuild()
+  {
+    if ($('#trelabels').length !== 0) return;
+
+    $('.header-search').after(button);
+
+    var trelabels = $('body').hasClass('trelabels');
+    if (typeof localStorage !== 'undefined') trelabels = localStorage.getItem('trelabels');
+    if (trelabels === "1") trelabels = true;
+
+    if (trelabels === true)
+      showLabels();
+    else
+      hideLabels();
+  }
+
   var target = document.querySelector('body');
-  
-  var observer = new MutationObserver(function(mutations) {
-    rebuild();
-  });
-  
+  var observer = new MutationObserver(function(mutations) {rebuild();});
   var config = {attributes: true};
   observer.observe(target, config);
-  
   rebuild();
-  
-  $('body').on('click', '#trelabels', function() {
-    if ($('body').hasClass('trelabels'))
-      $('#trelabels').attr('title', 'Show label names');
 
+  $('body').on('click', '#trelabels', function()
+  {
+    if ($('#trelabels').hasClass('active'))
+      hideLabels();
     else
-      $('#trelabels').attr('title', 'Hide label names');
-    
-    $('body').toggleClass('trelabels');
-    $('#trelabels').toggleClass('board-header-btn-enabled');
-    
+      showLabels();
+
     return true;
   });
 });
